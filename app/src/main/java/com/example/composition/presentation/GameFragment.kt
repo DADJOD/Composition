0fc30580a6911525360c83396f6856c3c7678db4
DADJOD <kamu57@mail.ru>
2024-10-13 17:36:08 +0300

    navigation with Fragment Manager

--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.composition.R
 import com.example.composition.databinding.FragmentGameBinding
+import com.example.composition.domain.entity.GameResult
+import com.example.composition.domain.entity.GameSettings
 import com.example.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
@@ -34,6 +36,19 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tvOption1.setOnClickListener {
+            launchGameFinishedFragment(
+                GameResult(
+                true,
+                0,
+                0,
+                GameSettings(
+                    0,
+                    0,
+                    0,
+                    0)
+            ))
+        }
     }
 
     override fun onDestroyView() {
@@ -45,8 +60,16 @@ class GameFragment : Fragment() {
         level = requireArguments().getSerializable(KEY_LEVEL) as Level
     }
 
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
     companion object {
 
+        const val NAME = "GameFragment"
         private const val KEY_LEVEL = "level"
 
         fun newInstance(level: Level): GameFragment {
